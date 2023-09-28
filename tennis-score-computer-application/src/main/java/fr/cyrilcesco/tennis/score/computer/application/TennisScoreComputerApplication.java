b75d10ac15ac122b26643e0d57745f3d63ba6035
CESCO Cyril <cyril.cesco@gmail.com>
2023-09-28 22:11:59 +0200

    use logger

--- tennis-score-computer-application/src/main/java/fr/cyrilcesco/tennis/score/computer/application/TennisScoreComputerApplication.java
+++ tennis-score-computer-application/src/main/java/fr/cyrilcesco/tennis/score/computer/application/TennisScoreComputerApplication.java
@@ -1,15 +1,19 @@
 package fr.cyrilcesco.tennis.score.computer.application;
 
 import fr.cyrilcesco.tennis.score.computer.application.service.ApplicationService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class TennisScoreComputerApplication {
 
+	private static final Logger logger = LoggerFactory.getLogger(TennisScoreComputerApplication.class);
+
 	public static void main(String[] args) {
 		ApplicationService service = new ApplicationService();
 		if(args.length >= 1) {
-			System.out.println(service.getDisplayTennisScoreComputer(args[0], args[1], args[2]));
+			logger.info(service.getDisplayTennisScoreComputer(args[0], args[1], args[2]));
 		}
 	}
 

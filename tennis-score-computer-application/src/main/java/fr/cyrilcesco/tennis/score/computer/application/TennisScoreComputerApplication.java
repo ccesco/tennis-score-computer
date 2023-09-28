4c737cb3400f3e96eb6bdae9c73488351dd01c82
CESCO Cyril <cyril.cesco@gmail.com>
2023-09-28 19:54:26 +0200

    init project with module domain and application

---
+++ tennis-score-computer-application/src/main/java/fr/cyrilcesco/tennis/score/computer/application/TennisScoreComputerApplication.java
@@ -0,0 +1,13 @@
+package fr.cyrilcesco.tennis.score.computer.application;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TennisScoreComputerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TennisScoreComputerApplication.class, args);
+	}
+
+}
